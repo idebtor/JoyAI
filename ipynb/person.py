0edb68da71e81b684d3262a6a0686723b44f4606
Youngsup Kim <idebtor@gmail.com>
2020-09-16 13:55:01 +0900

    up

---
+++ ipynb/person.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+from matplotlib.image import imread
+
+img = imread('https://github.com/idebtor/KMOOC-ML/blob/master/ipynb/images/person.png?raw=true')
+plt.imshow(img)
+plt.show()
